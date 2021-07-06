f2cee97d48572495197b121d496fc75b64866ec6
GitPit <rifflifter@gmail.com>
2021-07-06 17:38:09 -0400

    started seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (id, dept_name)
+VALUES (0, "general");
+
+INSERT INTO roles (id, title, salary, dept_id)
+VALUES (0, "employee)", 100000, 0);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (0, "John", "Doe", 0, 0);
