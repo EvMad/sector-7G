4009809edab1c32801dad9a6c4dd6e64bd62f7b8
GitPit <rifflifter@gmail.com>
2021-07-07 15:18:07 -0400

    added start script

--- seeds.sql
+++ seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO department (dept_name)
-VALUES ("general"), ("management"), ("software");
+VALUES ("general"), ("management"), ("software"), ("legal");
 
 INSERT INTO roles (title, salary)
 VALUES ("employee", 000000), ("manager", 150000), ("senior developer", 150000);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (0, "John", "Doe", 0, 0), (0, "Jane", "Doe", 2, 2);
+VALUES (1, "John", "Doe", "emp", "gen"), (0, "Jane", "Doe", "eng", "inf");
